dbda53bf65972ca22678dcfe8ceea0536a7d7198
Augy <ateoks@gmail.com>
2018-06-14 00:14:28 +0800

    Edited Fun.cs

---
+++ Modules/Fun.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Newtonsoft.Json.Linq;
+
+namespace ZeBot.Modules.Fun
+{
+
+}
