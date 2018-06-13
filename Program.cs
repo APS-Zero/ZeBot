0dad325df8ed95834974464d53c5abd4fe525259
APS-Zero <33553539+APS-Zero@users.noreply.github.com>
2018-06-13 19:07:48 +0300

    actually made it a functional core.net app
    shoulda done this sooner'

--- Program.cs
+++ Program.cs
@@ -1,42 +1,35 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using System.Threading.Tasks;
-namespace ZeBot
-{
-    public class ZeBot
-    {
-        static void Main() => new ZeBot().StartAsync().GetAwaiter().GetResult();
+namespace ZeBot {
+    public class ZeBot {
+        static void Main () => new ZeBot ().StartAsync ().GetAwaiter ().GetResult ();
 
         private DiscordSocketClient _client;
 
         private CommandHandler _handler;
-        public async Task StartAsync()
-        {
+        public async Task StartAsync () {
 
-            _client = new DiscordSocketClient();
+            _client = new DiscordSocketClient ();
 
-            await _client.LoginAsync(TokenType.Bot, "");
+            await _client.LoginAsync (TokenType.Bot, "");
 
-            await _client.StartAsync();
+            await _client.StartAsync ();
             _client.Ready += async () => {
                 Console.Title = "ZeBot (BETA)";
-                
+
                 Console.ForegroundColor = ConsoleColor.DarkBlue;
-                Console.Write("Logged in as ");
+                Console.Write ("Logged in as ");
                 Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("ZeBot.");
+                Console.WriteLine ("ZeBot.");
 
             };
 
+            _handler = new CommandHandler (_client);
 
-
-
-            _handler = new CommandHandler(_client);
-
-
-            await Task.Delay(-1);
+            await Task.Delay (-1);
 
         }
 
