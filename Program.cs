2916dacde02100303ae396aca2fa3eaff834644a
APS-Zero <33553539+APS-Zero@users.noreply.github.com>
2018-06-13 18:56:19 +0300

    added a program.cs
    just a normal program.cs wirh no token tho
    PS i already made a bot user and will get us a token in a sec lmao

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+namespace ZeBot
+{
+    public class ZeBot
+    {
+        static void Main() => new ZeBot().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+        public async Task StartAsync()
+        {
+
+            _client = new DiscordSocketClient();
+
+            await _client.LoginAsync(TokenType.Bot, "");
+
+            await _client.StartAsync();
+            _client.Ready += async () => {
+                Console.Title = "ZeBot (BETA)";
+                
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.Write("Logged in as ");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("ZeBot.");
+
+            };
+
+
+
+
+            _handler = new CommandHandler(_client);
+
+
+            await Task.Delay(-1);
+
+        }
+
+    }
+}
